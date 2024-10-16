4ea5bd8439a99acb9c2d1e2d350ad075eeeb3eee
Anish <anisharma030@gmail.com>
2024-10-16 19:43:32 +0530

    added image lazy loading, expand and download functionalities

--- lib/views/pages/chat_screen.dart
+++ lib/views/pages/chat_screen.dart
@@ -1,3 +1,4 @@
+import 'package:image_downloader/image_downloader.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
@@ -20,6 +21,53 @@ class ChatScreen extends StatelessWidget {
     required this.chatId,
     required this.localUser,
   });
+
+  void _showDownloadButton(BuildContext context, String imageUrl) {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          content: const Text('Do you want to download this image?'),
+          actions: [
+            TextButton(
+              onPressed: () => Navigator.pop(context), // Close dialog on cancel
+              child: const Text('Cancel'),
+            ),
+            TextButton(
+              onPressed: () async {
+                Navigator.pop(context); // Close the dialog
+                await _downloadImage(imageUrl); // Download the image
+              },
+              child: const Text('Download'),
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  Future<void> _downloadImage(String imageUrl) async {
+    try {
+      var imageId = await ImageDownloader.downloadImage(imageUrl);
+      if (imageId == null) {
+        return;
+      }
+
+      // Optionally, get the file path:
+      var filePath = await ImageDownloader.findPath(imageId);
+      Get.snackbar(
+        'Download complete',
+        'Image saved to $filePath',
+        snackPosition: SnackPosition.TOP,
+      );
+    } catch (error) {
+      Get.snackbar(
+        'Download failed',
+        'Failed to download the image.',
+        snackPosition: SnackPosition.BOTTOM,
+      );
+    }
+  }
 
   void _sendMessage() {
     final text = messageController.text;
@@ -78,37 +126,68 @@ class ChatScreen extends StatelessWidget {
                           );
                         }
                         final sender = userSnapshot.data;
-                        return message.text.startsWith('https://')
-                            ? Padding(
-                                padding: const EdgeInsets.symmetric(
-                                    vertical: 5, horizontal: 15.0),
-                                child: Row(
-                                  mainAxisAlignment:
-                                      user.uid == message.senderId
-                                          ? MainAxisAlignment.end
-                                          : MainAxisAlignment.start,
-                                  children: [
-                                    Container(
-                                      width: Get.width * 0.8,
-                                      decoration: BoxDecoration(
-                                        borderRadius: BorderRadius.circular(10),
-                                      ),
-                                      clipBehavior: Clip.hardEdge,
-                                      child: Image.network(
-                                        message.text,
-                                        fit: BoxFit.cover,
-                                        loadingBuilder:
-                                            (context, child, loadingProgress) =>
-                                                child,
-                                      ),
+                        if (message.text.startsWith('https://')) {
+                          return Padding(
+                            padding: const EdgeInsets.symmetric(
+                                vertical: 5, horizontal: 15.0),
+                            child: Row(
+                              mainAxisAlignment: user.uid == message.senderId
+                                  ? MainAxisAlignment.end
+                                  : MainAxisAlignment.start,
+                              children: [
+                                GestureDetector(
+                                  onTap: () {
+                                    _showImagePopup(
+                                      context,
+                                      message.text,
+                                    );
+                                  },
+                                  onLongPress: () {
+                                    _showDownloadButton(
+                                      context,
+                                      message.text,
+                                    );
+                                  },
+                                  child: Container(
+                                    width: Get.width * 0.8,
+                                    height: Get.width * 0.8,
+                                    decoration: BoxDecoration(
+                                      borderRadius: BorderRadius.circular(10),
                                     ),
-                                  ],
+                                    clipBehavior: Clip.hardEdge,
+                                    child: Image.network(
+                                      message.text,
+                                      fit: BoxFit.cover,
+                                      loadingBuilder:
+                                          (context, child, loadingProgress) {
+                                        if (loadingProgress == null) {
+                                          return child;
+                                        }
+                                        return Center(
+                                          child: CircularProgressIndicator(
+                                            value: loadingProgress
+                                                        .expectedTotalBytes !=
+                                                    null
+                                                ? loadingProgress
+                                                        .cumulativeBytesLoaded /
+                                                    loadingProgress
+                                                        .expectedTotalBytes!
+                                                : null,
+                                          ),
+                                        );
+                                      },
+                                    ),
+                                  ),
                                 ),
-                              )
-                            : MessageWidget(
-                                message: message,
-                                senderName: sender?.fullName ?? 'Unknown',
-                              );
+                              ],
+                            ),
+                          );
+                        } else {
+                          return MessageWidget(
+                            message: message,
+                            senderName: sender?.fullName ?? 'Unknown',
+                          );
+                        }
                       },
                     );
                   },
@@ -169,5 +248,32 @@ class ChatScreen extends StatelessWidget {
     } catch (e) {
       return null; // Handle errors appropriately
     }
+  }
+
+  void _showImagePopup(BuildContext context, String imageUrl) {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return Dialog(
+          backgroundColor: Colors.transparent,
+          child: ClipRRect(
+            borderRadius: BorderRadius.circular(10),
+            child: GestureDetector(
+              onTap: () => Navigator.pop(context),
+              child: InteractiveViewer(
+                panEnabled: true,
+                scaleEnabled: true,
+                minScale: 0.5,
+                maxScale: 4.0,
+                child: Image.network(
+                  imageUrl,
+                  fit: BoxFit.contain,
+                ),
+              ),
+            ),
+          ),
+        );
+      },
+    );
   }
 }
